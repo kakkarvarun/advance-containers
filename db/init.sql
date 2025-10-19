6032283dc437654b0d7e9e0c87f8ae495e2afd38
Varun Kakkar <kakkar.varun67@gmail.com>
2025-10-19 00:41:17 -0400

    feat(db): add init.sql to create users table

---
+++ db/init.sql
@@ -0,0 +1,6 @@
+-- Auto-runs on first DB initialization
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name  VARCHAR(100) NOT NULL
+);
